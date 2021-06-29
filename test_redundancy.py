17813cdf0a7c23d56d7587b5baacdf55bc73b20a
ahmedabdulwhhab <80491399+ahmedabdulwhhab@users.noreply.github.com>
2021-06-29 15:17:46 +0200

    Update test_redundancy.py

--- test_redundancy.py
+++ test_redundancy.py
@@ -11,15 +11,11 @@ if '__main__' == __name__:
     h1= net.addHost('h1',mac="00:00:00:00:00:01",ip="192.168.1.20/24")
     h2= net.addHost('h2',mac="00:00:00:00:00:02",ip="192.168.1.21/24")
     net.addLink(s1, h1)
-    net.addLink(s2, h1)
     net.addLink(s1, h2)
+    net.addLink(s2, h1)
     net.addLink(s2, h2)
-    net.addLink(s2, s1)
-    net.build()
-    c0.start()
-    s1.start([c0])
-    s2.start([c0])    
-    net.startTerms()
+    #net.addLink(s2, s1)
+
 
 
     h1.cmd('link set h1-eth1 down')
@@ -30,11 +26,17 @@ if '__main__' == __name__:
     h2.cmd('ip link set h2-eth1 address 00:00:00:00:00:12')
     h2.cmd('ip addr add 10.1.0.2/24 dev h2-eth1')
     h2.cmd('link set h2-eth1 up')
+    net.build()
+    c0.start()
+    s1.start([c0])
+    s2.start([c0])    
+    #net.startTerms()
     CLI(net)
     net.stop()    
     
     
 """
+ryu-manager ryu.app.simple_switch_stp_13
 h1 ip link set h1-eth1 down 
 h1 ip link set h1-eth1 address 00:00:00:00:00:11 
 h1 ip addr add 10.1.0.1/8 dev h1-eth1 
